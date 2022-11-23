d238ff3f68e458ceda1a901f4c42b17fd7531ad6
Atommerce <psw0962@naver.com>
2022-11-23 18:25:45 +0900

    add: mobile business info

--- component/common/app-wrapper.jsx
+++ component/common/app-wrapper.jsx
@@ -1,39 +1,39 @@
-import styled from "styled-components";
-import useThemeState from "@/hooks/useThemeState";
+import styled from 'styled-components';
+import useThemeState from '@/hooks/useThemeState';
 
 const AppWrapper = ({ children }) => {
   const { themeState } = useThemeState();
 
-  return (
-    <Frame themeState={themeState}>
-      {children}
-    </Frame>
-  )
-}
+  return <Frame themeState={themeState}>{children}</Frame>;
+};
 
 export default AppWrapper;
 
 const Frame = styled.div`
-    width: 100%;
-    height: 100%;
-    max-width: 450px;
-    min-height: 100vh;
-    margin: 0 auto;
-    box-shadow: ${props => props.themeState === 'dark' ? 'rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px' : 'rgb(0 0 0 / 16%) 0px 0px 8px'};
+  width: 100%;
+  height: 100%;
+  max-width: 450px;
+  min-height: 100vh;
+  margin: 0 auto;
+  box-shadow: ${props =>
+    props.themeState === 'dark'
+      ? 'rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px'
+      : 'rgb(0 0 0 / 16%) 0px 0px 8px'};
 
-    // top nav의 높이 + 40px
-    padding-top: 100px;
+  // top nav의 높이 + 40px
+  padding-top: 100px;
 
-    // bottom nav 높이만큼
-    padding-bottom: 65px;
+  // bottom nav 높이만큼
+  /* padding-bottom: 65px; */
+  padding-bottom: 0px;
 
-    // isLoading
-    ${props => {
-      if (props.isLoading) {
-        return css`
-          justify-content: center;
-          align-items: center;
-        `;
-      }
-    }}
+  // isLoading
+  /* ${props => {
+    if (props.isLoading) {
+      return css`
+        justify-content: center;
+        align-items: center;
+      `;
+    }
+  }} */
 `;
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ import event_1 from 'public/png/event/event_1.jpeg';
 import logo from 'public/png/logo.png';
 import FadeIn from 'react-fade-in';
 import Font from '@/component/common/font';
-import { motion, Variants } from 'framer-motion';
+import { motion } from 'framer-motion';
 import {
   main_1,
   main_2,
@@ -27,11 +27,22 @@ const mainImageData = [
   { id: 3, src: main_3 },
 ];
 
-const mainStepImageData = [
-  { id: 1, src: main_step_1 },
-  { id: 2, src: main_step_2 },
-  { id: 3, src: main_step_3 },
-];
+const cardVariants = {
+  offscreen: {
+    y: 300,
+  },
+  onscreen: {
+    y: 50,
+    rotate: -10,
+    transition: {
+      type: 'spring',
+      bounce: 0.4,
+      duration: 0.8,
+    },
+  },
+};
+
+const mainStepImageData = [[main_step_1], [main_step_2], [main_step_3]];
 
 const Home = () => {
   return (
@@ -54,21 +65,49 @@ const Home = () => {
         </div>
       </FadeIn>
 
-      <FadeIn>
-        <div>
-          <Font fontSize="2.5rem" margin="6rem 0 3rem 0">
-            남스짐의 차별화된 트레이닝 방식
-          </Font>
+      <Font fontSize="2.5rem" margin="6rem 0 3rem 0">
+        남스짐의 차별화된 트레이닝 방식
+      </Font>
 
-          {mainStepImageData.map((item, index) => {
-            return (
-              <CustomImageWrapper key={item.id}>
-                <Image src={item.src} alt={`main${index}`} />
-              </CustomImageWrapper>
-            );
-          })}
-        </div>
-      </FadeIn>
+      {mainStepImageData.map(([item], index) => (
+        <motion.div
+          key={index}
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.8 }}
+        >
+          <motion.div className="card" variants={cardVariants}>
+            <CustomImageWrapper>
+              <Image src={item} alt={`main${index}`} />
+            </CustomImageWrapper>
+          </motion.div>
+        </motion.div>
+      ))}
+
+      <FooterFrame>
+        <Font
+          fontSize="1.2rem"
+          fontWeight="700"
+          margin="0 0 2rem 0"
+        >{`상호명 : 남스짐 컴퍼니`}</Font>
+        <Font fontSize="1.2rem" fontWeight="700" margin="0 0 2rem 0">
+          사업자등록번호 : 000-00-00000
+        </Font>
+        <Font fontSize="1.2rem" fontWeight="700" margin="0 0 2rem 0">
+          대표자명 : 남건우
+        </Font>
+        <Font fontSize="1.2rem" fontWeight="700" margin="0 0 2rem 0">
+          주소 : 부천시 옥길동745-5 퀸즈파크 C동 617호
+        </Font>
+        <Font fontSize="1.2rem" fontWeight="700" margin="0 0 2rem 0">
+          전화번호 : 010-0000-0000 | 이메일 : admin@namsgym.com
+        </Font>
+        <Font fontSize="1.2rem" fontWeight="700" lineHeight="1.5">
+          *본 페이지에 제공되는 모든 이미지는 남스짐의 고유한 재산 입니다.
+          <br />
+          무단으로 도용시 법적 처벌을 요구할 수 있습니다.
+        </Font>
+      </FooterFrame>
     </Frame>
   );
 };
@@ -79,10 +118,23 @@ const Frame = styled.div`
   img {
     border-radius: 20px;
   }
+
+  .card {
+    margin: 0 auto;
+    width: 30rem;
+    height: 45rem;
+  }
 `;
 
 const CustomImageWrapper = styled(ImageWrapper)`
   border-radius: 20px;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
   margin-bottom: 1rem;
+`;
+
+const FooterFrame = styled.div`
+  padding: 1.5rem;
+  margin-top: 200px;
+  background-color: #f5f5f5;
+  border-radius: 20px;
 `;
