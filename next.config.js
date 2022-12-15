f032031f78390f0e7ed8c6242ae16440775d48f5
atommerce <psw0962@naver.com>
2022-12-15 11:15:38 +0900

    add: pwa

--- next.config.js
+++ next.config.js
@@ -1,29 +1,31 @@
 /** @type {import('next').NextConfig} */
 const withInterceptStdout = require('next-intercept-stdout');
-const withPlugins = require('next-compose-plugins');
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
-// intercept: recoil warning 무시 및 reactStrictMode
-const intercept = withInterceptStdout(
-  {
-    reactStrictMode: true,
-    // images: {
-    //   domains: [],
-    // },
+const nextConfig = {
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
   },
+  // images: {
+  //   domains: [],
+  // },
+};
 
-  text => (text.includes('Duplicate atom key') ? '' : text),
+const bulidConfig = () => {
+  const plugins = [withPWA];
+  const config = plugins.reduce((acc, plugin) => plugin(acc), {
+    ...nextConfig,
+  });
+
+  return config;
+};
+
+module.exports = withInterceptStdout(bulidConfig, text =>
+  text.includes('Duplicate atom key') ? '' : text,
 );
-
-// pwa
-const customWithPWA = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  buildExcludes: [/middleware-manifest.json$/],
-});
-
-// export
-module.exports = withPlugins([intercept, customWithPWA]);
