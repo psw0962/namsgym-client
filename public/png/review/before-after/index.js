6b75514b6f865b26f6feb1f90c4b2ee60da0dfbd
psw <psw0962@naver.com>
2023-02-17 20:15:15 +0900

    add: 2월 3주차 리뷰 추가

--- public/png/review/before-after/index.js
+++ public/png/review/before-after/index.js
@@ -1,4 +1,6 @@
 import before_after_1 from './before_after_1.png';
 import before_after_2 from './before_after_2.jpeg';
+import before_after_3 from './before_after_3.png';
+import before_after_4 from './before_after_4.jpeg';
 
-export { before_after_1, before_after_2 };
+export { before_after_1, before_after_2, before_after_3, before_after_4 };
