ea974bc124c645142f92e56f2b8d161a530736d9
psw <psw0962@naver.com>
2023-04-25 17:20:57 +0900

    add: 영상 추가

--- component/together/basic/BasicProgram.jsx
+++ component/together/basic/BasicProgram.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
-import Font from '@/component/common/font';
 
 const BasicProgram = ({ item, flag }) => {
   return (
-    <>
+    <Frame>
       {flag.flagNumber > 48 && (
         <>
           {item?.slice(0, 3).map((x, index) => {
+            console.log(x);
             return (
               <React.Fragment key={x?.id}>
-                <VideoWrapper>
-                  <CustomFont>{index + 1}</CustomFont>
-
+                {x.isRow ? (
+                  <video
+                    src={x?.image}
+                    poster="/png/logo.png"
+                    width={650}
+                    height={750}
+                    loop={true}
+                    autoPlay={true}
+                    muted={true}
+                  ></video>
+                ) : (
                   <video
                     src={x?.image}
                     poster="/png/logo.png"
@@ -22,7 +30,7 @@ const BasicProgram = ({ item, flag }) => {
                     autoPlay={true}
                     muted={true}
                   ></video>
-                </VideoWrapper>
+                )}
               </React.Fragment>
             );
           })}
@@ -34,19 +42,15 @@ const BasicProgram = ({ item, flag }) => {
           {item?.slice(3, 6).map((x, index) => {
             return (
               <React.Fragment key={x?.id}>
-                <VideoWrapper>
-                  <CustomFont>{index + 1}</CustomFont>
-
-                  <video
-                    src={x?.image}
-                    poster="/png/logo.png"
-                    width={450}
-                    height={750}
-                    loop={true}
-                    autoPlay={true}
-                    muted={true}
-                  ></video>
-                </VideoWrapper>
+                <video
+                  src={x?.image}
+                  poster="/png/logo.png"
+                  width={450}
+                  height={750}
+                  loop={true}
+                  autoPlay={true}
+                  muted={true}
+                ></video>
               </React.Fragment>
             );
           })}
@@ -58,42 +62,32 @@ const BasicProgram = ({ item, flag }) => {
           {item?.slice(6, 9).map((x, index) => {
             return (
               <React.Fragment key={x?.id}>
-                <VideoWrapper>
-                  <CustomFont>{index + 1}</CustomFont>
-
-                  <video
-                    src={x?.image}
-                    poster="/png/logo.png"
-                    width={450}
-                    height={750}
-                    loop={true}
-                    autoPlay={true}
-                    muted={true}
-                  ></video>
-                </VideoWrapper>
+                <video
+                  src={x?.image}
+                  poster="/png/logo.png"
+                  width={450}
+                  height={750}
+                  loop={true}
+                  autoPlay={true}
+                  muted={true}
+                ></video>
               </React.Fragment>
             );
           })}
         </>
       )}
-    </>
+    </Frame>
   );
 };
 
 export default BasicProgram;
 
-const VideoWrapper = styled.div`
-  position: relative;
-  border-radius: 20px;
-
-  .program {
-    border-radius: 20px;
-  }
+const Frame = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
-const CustomFont = styled(Font)`
-  position: absolute;
-  left: 3rem;
-  top: 1rem;
-  font-size: 10rem;
+const CustomVideo = styled.video`
+  width: 60rem;
+  height: 60rem;
 `;
